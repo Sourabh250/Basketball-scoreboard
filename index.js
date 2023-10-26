123bcecec8e15e4ec16f626741b5e30bdf97f973
Sourabh Banik <81009734+Sourabh250@users.noreply.github.com>
2023-10-26 17:03:40 +0530

    Updated js

--- index.js
+++ index.js
@@ -1,88 +1,83 @@
-let homeScore = 0
-let guestScore = 0
-let a = 10
-
-let homeScoreEl = document.getElementById("home-score-text")
-let guestScoreEl = document.getElementById("guest-score-text")
-let liveTextEl = document.getElementById("live-text")
-let homeTitleEl = document.getElementById("board-title-home")
-let guestTitleEl = document.getElementById("board-title-guest")
-let liveGameTextEl = document.getElementById("live-game-text")
-let timerEl = document.getElementById("timer")
-
-homeScoreEl.textContent = homeScore
-guestScoreEl.textContent = guestScore
-
-
-function addOneHome() {
-    homeScore += 1
-    homeScoreEl.textContent = homeScore
-    upDateLiveText()
+const Score = {
+    Home:  0,
+    Guest: 0
 }
 
-function addTwoHome() {
-    homeScore += 2
-    homeScoreEl.textContent = homeScore
-    upDateLiveText()
-}
+let timerDuration = 10
+const secondsInMinute = 60
+let interval
 
-function addThreeHome() {
-    homeScore += 3
-    homeScoreEl.textContent = homeScore
-    upDateLiveText()
-}
+const homeScoreEl = document.getElementById("home-score-text")
+const guestScoreEl = document.getElementById("guest-score-text")
+const liveTextEl = document.getElementById("live-text")
+const homeTitleEl = document.getElementById("board-title-home")
+const guestTitleEl = document.getElementById("board-title-guest")
+const liveGameTextEl = document.getElementById("live-game-text")
+const timerEl = document.getElementById("timer")
 
-function addOneGuest() {
-    guestScore += 1
-    guestScoreEl.textContent = guestScore
-    upDateLiveText()
-}
+homeScoreEl.textContent = Score.Home
+guestScoreEl.textContent = Score.Guest
 
-function addTwoGuest() {
-    guestScore += 2
-    guestScoreEl.textContent = guestScore
-    upDateLiveText()
-}
 
-function addThreeGuest() {
-    guestScore += 3
-    guestScoreEl.textContent = guestScore
+// function addHome(points) {
+//     homeScore += points
+//     homeScoreEl.textContent = homeScore
+//     upDateLiveText()
+// }
+
+// function addGuest(points) {
+//     guestScore += points
+//     guestScoreEl.textContent = guestScore
+//     upDateLiveText()
+// }
+
+//Modified to a single function
+
+function add(points, place) {
+    Score[place] += points
+    const scoreEl = document.getElementById(`${place.toLowerCase()}-score-text`)
+    scoreEl.textContent = Score[place]
     upDateLiveText()
 }
 
 function reset() {
-    liveGameTextEl.textContent = "Game In Progress"
-    homeScoreEl.textContent = 0
-    guestScoreEl.textContent = 0
-    homeScore = 0
-    guestScore = 0
-    homeTitleEl.style.color = "#EEEEEE"
-    guestTitleEl.style.color = "#EEEEEE"
-    liveTextEl.textContent = ""
-    
-    
-    const interval = setInterval(() => {
-            let minute = Math.trunc(a / 60)
-            let second = a % 60
-            timerEl.textContent = `${minute.toString().padStart(2, '0')}:${second.toString().padStart(2, '0')}`
-            a--
-            if (a < 0) {
-                clearInterval(interval);
-                a = 10;
-                liveGameTextEl.textContent = "Game Ended"
-                declareWinner()
-            }
-        }, 1000);
+    if (!interval) {
+        liveGameTextEl.textContent = "Game In Progress"
+        Score.Home = 0
+        Score.Guest = 0
+        homeScoreEl.textContent = Score.Home
+        guestScoreEl.textContent = Score.Guest
+        homeTitleEl.style.color = "#EEEEEE"
+        guestTitleEl.style.color = "#EEEEEE"
+        liveTextEl.textContent = ""
+        
+        
+        interval = setInterval(() => {
+                let minute = Math.trunc(timerDuration / secondsInMinute)
+                let second = timerDuration % secondsInMinute
+                timerEl.textContent = `${minute.toString().padStart(2, '0')}:${second.toString().padStart(2, '0')}`
+                timerDuration--
+                if (timerDuration < 0) {
+                    clearInterval(interval);
+                    interval = null
+                    timerDuration = 10;
+                    liveGameTextEl.textContent = "Game Ended"
+                    declareWinner()
+                }
+            }, 1000);
+    } else {
+        liveTextEl.textContent = "Cant Reset During Ongoing Gameplay"
+    }
 }
 
 
 
 function upDateLiveText() {
-    if (homeScore > guestScore) {
-        homeTitleEl.style.color = "#FE0139"
+    if (Score.Home > Score.Guest) {
+        homeTitleEl.style.color = "#F94F6D"
         guestTitleEl.style.color = "#EEEEEE"
-    } else if (homeScore < guestScore) {
-        guestTitleEl.style.color = "#FE0139"
+    } else if (Score.Home < Score.Guest) {
+        guestTitleEl.style.color = "#F94F6D"
         homeTitleEl.style.color = "#EEEEEE"
     } else {
         homeTitleEl.style.color = "#EEEEEE"
@@ -91,9 +86,9 @@ function upDateLiveText() {
 }   
 
 function declareWinner() {
-    if (homeScore > guestScore) {
+    if (Score.Home > Score.Guest) {
         liveTextEl.textContent = "Player One is the Winner"
-    } else if (homeScore < guestScore) {
+    } else if (Score.Home < Score.Guest) {
         liveTextEl.textContent = "Guest is the Winner"
     } else {
         liveTextEl.textContent = "Its a Draw!!!"
